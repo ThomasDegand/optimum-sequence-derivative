645427ee08c7b2f914a04e321975a2442a0bac6f
ThomasDegand <97782937+ThomasDegand@users.noreply.github.com>
2022-01-31 13:23:38 +0100

    Add files via upload

--- derivativeCoefficients.py
+++ derivativeCoefficients.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 M = 14
 CH = np.zeros((M,M))
 ONE = np.ones((M,1))
-mu = 0.271828182846
+mu = 0.27
 
 for i in range(1,M+1):
     for j in range(1,M+1):
@@ -31,11 +31,12 @@ def fbis(t):
 
 
 print(B)
-T = np.linspace(-2.5,2.5,1000)
+b = 2.5
+T = np.linspace(-b,b,1000)
 SH = fbis(T)
 print("p correlation: ", np.corrcoef(T,SH)[0][1])
 plt.plot(T,SH)
-plt.axis((-2.5,2.5,-2.5,2.5))
+plt.axis((-b,b,-b,b))
 plt.gca().set_aspect('equal', adjustable='box')
 plt.grid(color='k', linestyle='--', linewidth=0.5)
 plt.show()
@@ -48,8 +49,14 @@ def df(g,t):
         s += B[i-1]*g(t+i)/2
     return s
 
+def g0(t):
+    return np.sin(t)
+
+def dg0(t):
+    return np.cos(t)
+
 T2 = np.linspace(-10,10,8000)
-plt.plot(T2, np.cos(T2)) #sin derivative
-plt.plot(T2, df(np.sin,T2)) #estimated derivative
+plt.plot(T2, dg0(T2)) #derivative
+plt.plot(T2, df(g0,T2)) #estimated derivative
 plt.show()
-print("cos correlation: ", np.corrcoef(np.cos(T2),df(np.sin,T2))[0][1])
+print("derivative correlation: ", np.corrcoef(dg0(T2),df(g0,T2))[0][1])
